ae2f0128e86b3131476eff87052f197013503491
JJ <tangjun@zuzuche.com>
2019-10-30 16:10:50 +0800

    v0.4.0

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = app => {
   const { mongo = {} } = app.config;
   if (mongo.url && mongo.tables || mongo && mongo.clients) {
     const mongo = require('./lib/nobita-mongo');
-    mongo(app);
+    return mongo(app);
   }
 }
